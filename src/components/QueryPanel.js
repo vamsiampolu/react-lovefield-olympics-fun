5ae3b9b7afb271351101074f91d7ca721bc9e7a3
vamsiampolu <vamsideepak03@gmail.com>
2017-01-23 15:55:34 +1100

    Adds custom fields for Description, Title, Object along with a custom FieldTemplate

--- src/components/QueryPanel.js
+++ src/components/QueryPanel.js
@@ -1,17 +1,33 @@
 import React from 'react'
 import Form from 'react-jsonschema-form'
-import { schema, widgets } from '../schema'
+import { Button } from 'semantic-ui-react'
+import { schema, widgets, fields, FieldTemplate } from '../schema'
 
 export default class QueryPanel extends React.Component {
   constructor (props) {
     super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onSubmit (formData) {
+    console.log(formData)
   }
 
   render () {
+    const formData = {}
     return (<Form
+      className='ui form'
       schema={schema}
+      fields={fields}
+      formData={formData}
+      FieldTemplate={FieldTemplate}
       widgets={widgets}
-    />)
+      onSubmit={this.onSubmit}
+    >
+      <div>
+        <Button primary type='submit'>Submit</Button>
+      </div>
+    </Form>)
   }
 }
 
--- src/schema.js
+++ src/schema.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Dropdown } from 'semantic-ui-react'
+import { Form, Message, Header } from 'semantic-ui-react'
+import ObjectField from './components/ObjectField'
 export const schema = {
   title: 'Olympic Medal Search',
   description: 'Search for olympic medal winners by year, hosting city, country, event and more',
@@ -90,13 +91,40 @@ const Select = props => {
   console.log(props)
   const { label, options } = props
   const suiOptions = options.enumOptions.map(({ label, value }) => ({ text: label, value }))
-  return (<Dropdown selection text={label} options={suiOptions} />)
+  return (<Form.Select options={suiOptions} placeholder={label} />)
 }
 
-export const uiSchema = {
-  fromYear: {
-    'ui:widget': 'select'
+const DescriptionField = ({ id, description }) => {
+  if (description != null) {
+    return (<Message content={description} />)
   }
+  return null
+}
+
+const TitleField = ({ id, required, title }) => {
+  const size = id !== 'root__title' ? 'medium' : 'large'
+  return (<Header size={size}>{title}</Header>)
+}
+
+export const FieldTemplate = props => {
+  const { id, label, help, required, description, errors, children } = props
+  console.log(id)
+  const desc = id !== 'root' ? description : null
+  const labelComp = id !== 'root' ? <label>{label}</label> : null
+  const classNames = `${required ? 'required ' : ''}field`
+  return (<div className={classNames}>
+    {labelComp}
+    {desc}
+    {children}
+    {errors}
+    {help}
+  </div>)
+}
+
+export const fields = {
+  DescriptionField: DescriptionField,
+  TitleField: TitleField,
+  ObjectField: ObjectField
 }
 
 export const widgets = { 'select': Select }
